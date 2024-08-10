814d5733119495f2b502e8584ed69625798b74dd
mukeshwagh9284 <147371611+mukeshwagh9284@users.noreply.github.com>
2024-08-10 23:37:10 +0530

    Update ResetPassword.jsx

--- auth/src/components/ResetPassword.jsx
+++ auth/src/components/ResetPassword.jsx
@@ -11,7 +11,7 @@ const ResetPassword = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `auth-app-api-peach.vercel.app/user/reset-password/${token}`,
+        `https://auth-app-api-peach.vercel.app/user/reset-password/${token}`,
         { password }
       );
       setMessage(response.data.message);
